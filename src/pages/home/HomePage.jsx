bc0133046bf098dcd94e2130a55e00091e1dc924
SlavaProksimo <slavikchan167@gmail.com>
2026-04-27 22:05:28 +0300

    3

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import ButtonAddTodo from "@/ui/button/ButtonAddTodo";
-import TodoAdd from "@/ui/todo-add/TodoAdd";
+import TodoAdd from "@/ui/todo-add/todoadd";
 import NewTasks from "@/ui/new-tasks/NewTasks";
 import Search from "@/ui/search/Search";
 import NotFound from "@/ui/not-found/NotFound";
